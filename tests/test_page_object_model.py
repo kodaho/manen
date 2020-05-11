025129020b67231abbb63634385be40abb6d7ecd
kodaho <23268819+kodaho@users.noreply.github.com>
2020-05-11 17:25:18 +0200

    Add tests for page_object_module

---
+++ tests/test_page_object_model.py
@@ -0,0 +1,107 @@
+from collections import namedtuple
+from unittest.mock import MagicMock, PropertyMock
+
+from selenium.webdriver.common.by import By
+
+from manen.page_object_model import (
+    Element,
+    Elements,
+    InputElement,
+    Page,
+    TextElement,
+    TextElements,
+)
+
+ExpectedResult = namedtuple("ExpectedResult", ("attr", "strategy", "selector"))
+
+
+class TestElementInPage:
+    @staticmethod
+    def get_test_context(element_class):
+        class ManenTestPage(Page):
+            css = element_class("css:div")
+            xpath = element_class("xpath://div")
+            post_processing = element_class(
+                "css:div", post_processing=lambda x: x.extract()
+            )
+
+        expected_results = (
+            ExpectedResult(attr="css", strategy=By.CSS_SELECTOR, selector="div"),
+            ExpectedResult(attr="xpath", strategy=By.XPATH, selector="//div"),
+        )
+
+        return ManenTestPage, expected_results
+
+    def test_can_access_element_in_page(self):
+        browser = MagicMock()
+        page_class, expected_results = self.get_test_context(Element)
+        page = page_class(browser)
+
+        for attribute, strategy, selector in expected_results:
+            _ = getattr(page, attribute)
+            browser.find_element.assert_called_with(strategy, selector)
+            browser.reset_mock()
+
+    def test_can_access_elements_in_page(self):
+        browser = MagicMock()
+        page_class, expected_results = self.get_test_context(Elements)
+        page = page_class(browser)
+
+        for attribute, strategy, selector in expected_results:
+            _ = getattr(page, attribute)
+            browser.find_elements.assert_called_with(strategy, selector)
+            browser.reset_mock()
+
+    def test_can_acess_text_element_in_page(self):
+        browser = MagicMock()
+        mock_element, mock_text = MagicMock(), PropertyMock()
+        type(mock_element).text = mock_text
+        browser.find_element = MagicMock(return_value=mock_element)
+
+        page_class, expected_results = self.get_test_context(TextElement)
+        page = page_class(browser)
+
+        for attribute, strategy, selector in expected_results:
+            _ = getattr(page, attribute)
+            browser.find_element.assert_called_with(strategy, selector)
+            mock_text.assert_called_once()
+            browser.reset_mock()
+            mock_text.reset_mock()
+
+    def test_can_acess_text_elements_in_page(self):
+        browser = MagicMock()
+        mock_element, mock_text = MagicMock(), PropertyMock()
+        type(mock_element).text = mock_text
+        browser.find_elements = MagicMock(return_value=mock_element)
+
+        page_class, expected_results = self.get_test_context(TextElements)
+        page = page_class(browser)
+
+        for attribute, strategy, selector in expected_results:
+            _ = getattr(page, attribute)
+            browser.find_elements.assert_called_with(strategy, selector)
+            mock_text.assert_called_once()
+            browser.reset_mock()
+            mock_text.reset_mock()
+
+    def test_can_set_input_in_page(self):
+        class ManenTestPage(Page):
+            email = InputElement("css:input[name='email']")
+
+        browser = MagicMock()
+        page = ManenTestPage(browser)
+        page.email = "this is a test"
+
+        browser.find_element.assert_called_with(By.CSS_SELECTOR, "input[name='email']")
+        browser.find_element.return_value.send_keys.assert_called_with("this is a test")
+
+    def test_can_get_input_in_page(self):
+        class ManenTestPage(Page):
+            email = InputElement("css:input[name='email']")
+
+        browser = MagicMock()
+        page = ManenTestPage(browser)
+        _ = page.email
+
+        browser.find_element.assert_called_with(By.CSS_SELECTOR, "input[name='email']")
+        browser.find_element.return_value.get_attribute.assert_called_with("value")

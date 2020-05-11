57f4098dc412ce75fafbdc62c7e2c13d94371b9e
kodaho <23268819+kodaho@users.noreply.github.com>
2020-05-11 22:45:54 +0200

    Add documentation in page_object_model and home

--- manen/__init__.py
+++ manen/__init__.py
@@ -1,16 +1,19 @@
-"""``manen`` is a module to ease the interaction with Selenium. Its features
+""":py:mod:`manen` is a module to ease the interaction with Selenium. Its features
 are splitted into four modules, where each module is independant:
 
-- ``manen.resource`` : set of helpers to get the drivers needed by Selenium to work
-- ``manen.finder``: define a function `find` in order to avoid calling the methods
-  ``find_element`` or ``find_elements`` of a
+- :py:mod:`manen.resource` : set of helpers to get the drivers needed by Selenium to work
+- :py:mod:`manen.finder`: define a function :py:func:`~manen.finder.find` in order to
+  avoid calling the methods ``find_element`` or ``find_elements`` of a
   :py:class:`Selenium WebElement <selenium.webdriver.remote.webelement.WebElement>`
-- ``manen.navigator``: a wrapper around `selenium.webdriver.remote.webdriver.WebDriver`
-  to create more easily instance of web drivers. Compatible with Chrome and Firefox
-- ``manen.page_object_model``: an implementation of Page Objects Model in order to easily
-  interact with the DOM of a webpage of the DOM structure
+- :py:mod:`manen.navigator`: a wrapper around
+  :py:class:`~selenium.webdriver.remote.webdriver.WebDriver` to more easily create
+  instance of web drivers. Onyl compatible with Chrome (for now).
+- :py:mod:`manen.page_object_model`: an implementation of the design pattern Page
+  Objects Model in order to easily interact with the DOM of a webpage.
 
-Given that each module are independant, you can use one of the module without another one.
+Each module are built to be quite independant from each other. For example, you can
+use the module :py:mod:`manen.finder` without :py:mod:`manen.navigator` or
+:py:mod:`manen.page_object_model` without :py:mod:`manen.navigator`.
 """
 
 __version__ = "0.1.0"
--- manen/page_object_model.py
+++ manen/page_object_model.py
@@ -1,16 +1,17 @@
 # pylint: disable=too-few-public-methods
 """This module provides an implementation of the `Page Object design pattern
 <https://www.selenium.dev/documentation/en/guidelines_and_recommendations/page_object_models/>`_
-described in Selenium documentation. By combining the classes ``Page`` and
-``Region`` with the ``Element`` object (and all its subclasses), you can
-easily describe any web pages and access all the DOM elements in a simple
+described in Selenium documentation. By combining the classes
+:py:class:`manen.page_object_model.Page` and :py:class:`manen.page_object_model.Region`
+with the :py:class:`manen.page_object_model.Element` object (and all its subclasses),
+you can easily describe any web pages and access all the DOM elements in a simple
 way through a Python class.
 
-Let's say you want to work with `manen` on the website `PyPi <https://pypi.org/>`_
-to get all the packages informations matching a given query (e.g.: selenium).
-The first step to work with `manen.dom` is to define the Python classes which
+Let's say you want, given a query, to get all the packages information from
+the website `PyPi <https://pypi.org/>`_ (e.g.: "selenium"). The first step to
+work with :py:mod:`manen.page_object_model` is to define the Python classes which
 will describe the web pages you are working on. Here we will define all the
-classes in an external file called `pypi_pom.py`.
+classes in an external file called ``pypi_pom.py``.
 
 .. code-block:: python
 
@@ -27,14 +28,16 @@ classes in an external file called `pypi_pom.py`.
             link = LinkElement("a.package-snippet")
             description = TextElement("p.package-snippet__description")
 
-        n_results = FigureElement("//*[@id='content']//form/div[1]/div[1]/p/strong")x
+        n_results = FigureElement("//*[@id='content']//form/div[1]/div[1]/p/strong")
         results = ResultRegions("ul[aria-label='Search results'] li")
 
 
 Once you have defined all the classes describing the web pages, you can start
-interacting by instanciating the `Page` subclasses with an instance of `WebDriver`.
-Here we will suppose that you have an instance of `WebDriver` stored in the variable
-`browser`.
+interacting by instanciating the :py:class:`~manen.page_object_model.Page` subclass
+with an instance of :py:class:`~selenium.webdriver.remote.webdriver.WebDriver`. Here
+we will suppose that you have an instance of
+:py:class:`~selenium.webdriver.remote.webdriver.WebDriver` stored in the variable
+``browser``.
 
 .. code-block:: python
 
@@ -59,7 +62,7 @@ Here we will suppose that you have an instance of `WebDriver` stored in the vari
     >>> page.results[0].link
     "https://pypi.org/project/selenium/"
 
-This is a glitch of what you can do with `manen.page_object_model`. See the
+This is a glitch of what you can do with :py:mod:`manen.page_object_model`. See the
 documentation of each objects to check all the features provided by the module.
 """
 from contextlib import contextmanager
@@ -83,11 +86,10 @@ __all__ = (
     "Action",
     "Element",
     "Elements",
-    "FigureElement",
-    "FigureElements",
     "Frame",
-    "Frames",
     "InputElement",
+    "IntegerElement",
+    "IntegerElements",
     "LinkElement",
     "LinkElements",
     "Page",
@@ -100,11 +102,11 @@ __all__ = (
 
 
 class WebArea:
-    """A `WebArea` element is a wrapper around a Selenium element which works
-    together with an `Element` (and its subclasses). Any class attribute defined
-    with the class `Element` in a class which inherits from `WebArea` will be
-    accessible as an instance attribute; it will, when accessed, evaluate the
-    selector to retrieve the element.
+    """Wrapper around a Selenium element which works together with an
+    :py:class:`~manen.page_object_model.Element` (and its subclasses). Any class
+    attribute instanciated with :py:class:`~manen.page_object_model.Element`
+    will be accessible as an instance attribute; it will, when accessed, evaluate
+    the selector to retrieve the element.
 
     Example::
 
@@ -112,19 +114,27 @@ class WebArea:
             body = Element("css:body")
             first_div = Element("css:div")
             links = Elements("css:a")
+
         >>> area = Area(browser)
         >>> area.body
         <selenium.webdriver.remote.webelement.WebElement id="abcdef-1234">
         >>> div_area = Area(area.first_div)
         >>> div_area.links # Find all the links in the first div element of the body
 
-    .. note:: This class is mainly used by `manen` to build the classes `Page`,
-       `Region` and `Frame`. You shouldn't need to use this class to create
-       basic page objects (see the documentation of `Page`, `Region` and `Frame`
-       which are the main objects for page object modelling).
+    .. note:: This class is mainly used internally to build the classes
+      :py:class:`~manen.page_object_model.Page`,
+      :py:class:`~manen.page_object_model.Region` and
+      :py:class:`~manen.page_object_model.Frame`. You shouldn't need to use this
+      class to create basic page objects (see the documentation of each object to
+      see how to efficiently used page object modelling).
     """
 
     def __init__(self, container: "SeleniumElement", _context: str = "PAGE"):
+        """
+        Args:
+            container ("SeleniumElement"): Parent element in which all the
+                elements will be searched.
+        """
         self._container = container
         self._context = _context
         self._driver: "WebDriver" = getattr(container, "parent", container)
@@ -162,6 +172,8 @@ class WebArea:
 
 
 class DomAccessor:
+    """Main interface of a DOM element."""
+
     _post_processing: List["PostProcessingFunction"] = []
     _many = False
 
@@ -243,34 +255,79 @@ class DomAccessor:
 
 
 class Page(WebArea):
+    """Modelize a HTML page in the Page Object Model design pattern. The attributes of
+    :py:class:`~manen.page_object_model.Page` should be an instance of
+    :py:class:`~manen.page_object_model.Element` (or any subclass) or an instance of
+    :py:class:`~manen.page_object_model.Region`.
+
+    Example::
+
+        class QueryPage(Page):
+            main_title = TextElement("h1")
+            search = InputElement("input[name='query']")
+            button_validate = Element("button#validate")
+
+        >>> page = QueryPage(browser)
+        >>> page.search = "python manen"
+        >>> button_validate.click()
+    """
+
     @property
     def title(self) -> str:
+        """Title of the page."""
         return self._container.title
 
     @property
     def page_source(self) -> str:
+        """Code source of the page."""
         return self._container.page_source
 
-    def click_with_js(self, element):
+    def click_with_js(self, element: "WebElement"):
+        """Click on an element using a JavaScript script."""
         return self._container.click_with_js(element)
 
 
 class Region(DomAccessor):
-    def __get__(self, area, area_cls) -> WebArea:
+    """Modelize a region of a webpage. A region is a set of elements contained
+    in the same container.
+
+    .. note:: Contrary to :py:class:`~manen.page_object_model.Page`, this class
+       should not be instanciated with an instance of WebDriver. This class is
+       closer to an :py:class:`~manen.page_object_model.Element` than a
+       :py:class:`~manen.page_object_model.Element`.
+
+    Example::
+
+        class LoginPage(Page):
+            class FormRegion(Region):
+                email = InputElement("input#email")
+                password = InputElement("input#password")
+
+            form_region = FormRegion("div#form-container")
+
+        >>> page = LoginPage(browser)
+        >>> page.form_region.email = "hello@manen.com"
+        >>> page.form_region.password = "strong_password"
+        >>> page.form_region.password = Action("submit")
+    """
+
+    def __get__(self, area: WebArea, area_cls) -> WebArea:
         element = super()._get_from(area, area_cls)
         element = self._build_elements(element, context="REGION")
         assert isinstance(element, WebArea)  # For mypy
         return element
 
-    def __set__(self, instance, value):
+    def __set__(self, area: WebArea, value: Any):
         raise UnsettableElement(self.__class__.__name__)
 
 
 class Regions(Region, many=True):
-    pass
+    """Pluralized version of :py:class:`manen.page_object_model.Region`."""
 
 
 class Frame(DomAccessor):
+    """Enable to work with <iframe> element."""
+
     def __get__(self, area, area_cls) -> WebArea:
         element = super()._get_from(area, area_cls)
         element = self._build_elements(element, context="FRAME")
@@ -281,60 +338,80 @@ class Frame(DomAccessor):
         raise UnsettableElement(self.__class__.__name__)
 
 
-class Frames(Frame, many=True):
-    pass
-
-
 class Action:
-    def __init__(self, method: Callable, *args, **kwargs):
+    """Enable to interact with an input by calling a method of the WebElement. This is
+    intended to be used by a subtype of :py:class:`~manen.page_object_model.Element`
+    which can be set.
+
+    Example::
+
+        >>> page.query = "python manen"
+        >>> page.query = Action("submit")
+    """
+
+    def __init__(self, method: str, *args, **kwargs):
+        """
+        Args:
+            method (str): name of the method to call. This should be a method ones
+                of a method of :py:class:`selenium.webdriver.remote.webelement.WebElement`
+            *args (Any): positional arguments called with the method
+            **kwargs : keyword arguments called with the method
+        """
         self.method = method
         self.args = args
         self.kwargs = kwargs
 
 
 class Element(DomAccessor):
-    """Must be placed in a class based on WebArea.
-    Returned a Selenium WebElement.
+    """Based on one or several selectors, extract a
+    :py:class:`selenium.webdriver.remote.webelement.WebElement` from a region or page.
+
+    An element should be used to initialize a class attribute of a subclass of
+    :py:class:`manen.page_object_model.Page`, :py:class:`manen.page_object_model.Region`
+    or :py:class:`manen.page_object_model.Frame` (or any subtypes of
+    :py:class:`manen.page_object_model.WebArea`)
     """
 
-    def __get__(self, area, area_cls) -> "WebElement":
+    def __get__(self, area: WebArea, area_cls: type) -> "WebElement":
         return super()._get_from(area, area_cls)
 
-    def __set__(self, page, value):
+    def __set__(self, area: WebArea, value: Any):
         raise UnsettableElement(self.__class__.__name__)
 
 
 class Elements(Element, many=True):
-    pass
+    """Pluralized version of :py:class:`manen.page_object_model.Element`."""
 
 
 class LinkElement(Element, post_processing=[lambda x: x.get_attribute("href")]):
-    pass
+    """Extract the link from an element matching one or several selectors."""
 
 
 class LinkElements(  # pylint: disable=bad-continuation
     LinkElement, many=True, post_processing=[lambda x: x.get_attribute("href")],
 ):
-    pass
+    """Pluralized version of :py:class:`manen.page_object_model.LinkElement`."""
 
 
 class TextElement(Element, post_processing=[lambda x: x.text]):
-    pass
+    """Extract text from an element matching one or several selectors."""
 
 
 class TextElements(TextElement, many=True, post_processing=[lambda x: x.text]):
-    pass
+    """Pluralized version of :py:class:`manen.page_object_model.TextElement`."""
 
 
-class FigureElement(Element, post_processing=[lambda x: x.text, extract_integer]):
-    pass
+class IntegerElement(Element, post_processing=[lambda x: x.text, extract_integer]):
+    """Extract the integer from a text element matching a selector or set of selectors.
+    """
 
 
-class FigureElements(  # pylint: disable=bad-continuation
+class IntegerElements(  # pylint: disable=bad-continuation
     Element, many=True, post_processing=[lambda x: x.text, extract_integer]
 ):
     """Get elements from a HTML page matching a set of selectors and extract a
-    figure from each text element.
+    integer from each text element.
+    Pluralized version of :py:class:`manen.page_object_model.IntegerElement`
     """
 
 
@@ -347,6 +424,7 @@ class InputElement(Element, post_processing=[lambda x: x.get_attribute("value")]
         class LoginPage(Page):
             email = InputElement("input[type='email']")
             password = InputElement("input[type='password']")
+
         >>> from manen.page_object_model import Action
         >>> login_page = LoginPage(browser)
         >>> login_page.email = "username@manen.co"
@@ -354,11 +432,11 @@ class InputElement(Element, post_processing=[lambda x: x.get_attribute("value")]
         >>> login_page.password = Action("submit")
     """
 
-    def __set__(self, area, value):
+    def __set__(self, area: WebArea, value: Any):
         with area.switch_container() as container:
             input_ = find(self.selectors, inside=container)
             if isinstance(value, Action):
-                getattr(input_, value.method_to_call)(*value.args, **value.kwargs)
+                getattr(input_, value.method)(*value.args, **value.kwargs)
             else:
                 input_.clear()
                 input_.send_keys(value)

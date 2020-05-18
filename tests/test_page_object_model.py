0091169a57ca135cac14a971ce59a5bcdb474241
kodaho <23268819+kodaho@users.noreply.github.com>
2020-05-18 20:33:18 +0200

    Improve tests for page_object_model

--- tests/test_page_object_model.py
+++ tests/test_page_object_model.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from datetime import datetime
 from pathlib import Path
 from unittest.mock import MagicMock, PropertyMock
 
@@ -8,10 +9,15 @@ from selenium.webdriver.common.by import By
 import pytest
 from manen.exceptions import ManenException
 from manen.page_object_model import (
+    DatetimeElement,
+    DatetimeElements,
     Element,
-    Elements,
+    ImageSourceElement,
+    ImageSourceElements,
     InputElement,
     IntegerElement,
+    IntegerElements,
+    LinkElement,
     LinkElements,
     Page,
     PageObjectLoader,
@@ -23,75 +29,135 @@ from manen.page_object_model import (
 ExpectedResult = namedtuple("ExpectedResult", ("attr", "strategy", "selector"))
 
 
+@pytest.mark.parametrize(
+    "element_class", [Element, TextElement, LinkElement, ImageSourceElement],
+)
+@pytest.mark.parametrize(
+    "manen_selector,strategy,selector",
+    [
+        ("css:div", By.CSS_SELECTOR, "div"),
+        ("div", By.CSS_SELECTOR, "div"),
+        ("xpath://div", By.XPATH, "//div"),
+        ("xp://div", By.XPATH, "//div"),
+        ("//div", By.XPATH, "//div"),
+        (".//div", By.XPATH, ".//div"),
+        ("./div", By.XPATH, "./div"),
+        ("class_name:my-class", By.CLASS_NAME, "my-class"),
+        ("class:my-class", By.CLASS_NAME, "my-class"),
+        ("cls:my-class", By.CLASS_NAME, "my-class"),
+        ("tag_name:a", By.TAG_NAME, "a"),
+        ("tag:a", By.TAG_NAME, "a"),
+        ("partial_link_text:a", By.PARTIAL_LINK_TEXT, "a"),
+        ("plink:a", By.PARTIAL_LINK_TEXT, "a"),
+        ("id:a", By.ID, "a"),
+    ],
+)
+def test_choose_right_selection_method_for_element(
+    element_class, manen_selector, strategy, selector
+):
+    class ManenTestPage(Page):
+        attr = element_class(manen_selector)
+
+    browser = MagicMock()
+    page = ManenTestPage(browser)
+    _ = page.attr
+    browser.find_element.assert_called_once_with(strategy, selector)
+
+
+@pytest.mark.parametrize(
+    "element_class,text_value,expected_value",
+    [
+        (TextElement, "this is a text", "this is a text"),
+        (IntegerElement, "3 values", 3),
+        (DatetimeElement, "2020/05/01 at 12:00", datetime(2020, 5, 1, 12, 00)),
+    ],
+)
+def test_post_processing_of_text_element(element_class, text_value, expected_value):
+    class ManenTestPage(Page):
+        attr = element_class("selector")
+
+    browser = MagicMock()
+    mock_element, mock_text = MagicMock(), PropertyMock(return_value=text_value)
+    type(mock_element).text = mock_text
+    browser.find_element = MagicMock(return_value=mock_element)
+    page = ManenTestPage(browser)
+    assert page.attr == expected_value
+
+
+@pytest.mark.parametrize(
+    "element_class,values,expected_values",
+    [
+        (
+            TextElements,
+            ["this is a text", "this is an other one", "and a last one"],
+            ["this is a text", "this is an other one", "and a last one"],
+        ),
+        (
+            IntegerElements,
+            ["3 values", "2 values", "just 1 value", "and 10,000 others"],
+            [3, 2, 1, 10000],
+        ),
+        (DatetimeElements, ["2020/05/01 at 12:00"], [datetime(2020, 5, 1, 12, 00)]),
+    ],
+)
+def test_post_processing_of_text_elements(element_class, values, expected_values):
+    class ManenTestPage(Page):
+        attr = element_class("selector")
+
+    browser = MagicMock()
+    find_elements_returned_value = []
+    for value in values:
+        mock_element, mock_text = MagicMock(), PropertyMock(return_value=value)
+        type(mock_element).text = mock_text
+        find_elements_returned_value.append(mock_element)
+    browser.find_elements = MagicMock(return_value=find_elements_returned_value)
+    page = ManenTestPage(browser)
+    assert page.attr == expected_values
+
+
+@pytest.mark.parametrize(
+    "element_class,attribute,value",
+    [
+        (ImageSourceElement, "src", "http://www.example.com/assets/img.jpg"),
+        (LinkElement, "href", "http://www.example.com"),
+        (InputElement, "value", "email@manen.co"),
+    ],
+)
+def test_post_processing_of_element_with_attribute(element_class, attribute, value):
+    class ManenTestPage(Page):
+        attr = element_class("selector")
+
+    browser = MagicMock()
+    browser.find_element.return_value.get_attribute.return_value = value
+    page = ManenTestPage(browser)
+    assert page.attr == value
+    browser.find_element.return_value.get_attribute.assert_called_once_with(attribute)
+
+
+@pytest.mark.parametrize(
+    "element_class,attribute,values",
+    [
+        (ImageSourceElements, "src", ["http://www.example.com/assets/img.jpg"]),
+        (LinkElements, "href", ["http://www.example.com"]),
+    ],
+)
+def test_post_processing_of_elements_with_attribute(element_class, attribute, values):
+    class ManenTestPage(Page):
+        attr = element_class("selector")
+
+    browser = MagicMock()
+    browser.find_elements.return_value = []
+    for value in values:
+        element = MagicMock()
+        element.get_attribute.return_value = value
+        browser.find_elements.return_value.append(element)
+    page = ManenTestPage(browser)
+    assert page.attr == values
+    for element in browser.find_elements.return_value:
+        element.get_attribute.assert_called_once_with(attribute)
+
+
 class TestElementInPage:
-    @staticmethod
-    def get_test_context(element_class):
-        class ManenTestPage(Page):
-            css = element_class("css:div")
-            xpath = element_class("xpath://div")
-            post_processing = element_class(
-                "css:div", post_processing=lambda x: x.extract()
-            )
-
-        expected_results = (
-            ExpectedResult(attr="css", strategy=By.CSS_SELECTOR, selector="div"),
-            ExpectedResult(attr="xpath", strategy=By.XPATH, selector="//div"),
-        )
-
-        return ManenTestPage, expected_results
-
-    def test_can_access_element_in_page(self):
-        browser = MagicMock()
-        page_class, expected_results = self.get_test_context(Element)
-        page = page_class(browser)
-
-        for attribute, strategy, selector in expected_results:
-            _ = getattr(page, attribute)
-            browser.find_element.assert_called_with(strategy, selector)
-            browser.reset_mock()
-
-    def test_can_access_elements_in_page(self):
-        browser = MagicMock()
-        page_class, expected_results = self.get_test_context(Elements)
-        page = page_class(browser)
-
-        for attribute, strategy, selector in expected_results:
-            _ = getattr(page, attribute)
-            browser.find_elements.assert_called_with(strategy, selector)
-            browser.reset_mock()
-
-    def test_can_acess_text_element_in_page(self):
-        browser = MagicMock()
-        mock_element, mock_text = MagicMock(), PropertyMock()
-        type(mock_element).text = mock_text
-        browser.find_element = MagicMock(return_value=mock_element)
-
-        page_class, expected_results = self.get_test_context(TextElement)
-        page = page_class(browser)
-
-        for attribute, strategy, selector in expected_results:
-            _ = getattr(page, attribute)
-            browser.find_element.assert_called_with(strategy, selector)
-            mock_text.assert_called_once()
-            browser.reset_mock()
-            mock_text.reset_mock()
-
-    def test_can_acess_text_elements_in_page(self):
-        browser = MagicMock()
-        mock_element, mock_text = MagicMock(), PropertyMock()
-        type(mock_element).text = mock_text
-        browser.find_elements = MagicMock(return_value=mock_element)
-
-        page_class, expected_results = self.get_test_context(TextElements)
-        page = page_class(browser)
-
-        for attribute, strategy, selector in expected_results:
-            _ = getattr(page, attribute)
-            browser.find_elements.assert_called_with(strategy, selector)
-            mock_text.assert_called_once()
-            browser.reset_mock()
-            mock_text.reset_mock()
-
     def test_can_set_input_in_page(self):
         class ManenTestPage(Page):
             email = InputElement("css:input[name='email']")
@@ -102,17 +168,6 @@ class TestElementInPage:
 
         browser.find_element.assert_called_with(By.CSS_SELECTOR, "input[name='email']")
         browser.find_element.return_value.send_keys.assert_called_with("this is a test")
-
-    def test_can_get_input_in_page(self):
-        class ManenTestPage(Page):
-            email = InputElement("css:input[name='email']")
-
-        browser = MagicMock()
-        page = ManenTestPage(browser)
-        _ = page.email
-
-        browser.find_element.assert_called_with(By.CSS_SELECTOR, "input[name='email']")
-        browser.find_element.return_value.get_attribute.assert_called_with("value")
 
 
 class TestLoadPage:

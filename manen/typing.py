734d036d1a3c3b0fc6b9c3c118deff0c0ebc3a31
kodaho <23268819+kodaho@users.noreply.github.com>
2022-01-23 17:13:34 +0100

    Fix typing system

--- manen/typing.py
+++ manen/typing.py
@@ -6,54 +6,57 @@ Describe some common types used by :py:mod:`manen`.
 """
 
 from datetime import date
-from typing import TYPE_CHECKING
+from typing import Any, Dict, Tuple, TypedDict, Union
 
-if TYPE_CHECKING:
-    from typing import Any, Dict, Tuple, TypedDict, Union
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from typing_extensions import Protocol
 
-    from selenium.webdriver.remote.webdriver import WebDriver
-    from selenium.webdriver.remote.webelement import WebElement
-    from typing_extensions import Protocol
+SeleniumElement = Union[WebDriver, WebElement]
+Version = Tuple[int, int, int, int]
 
-    SeleniumElement = Union[WebDriver, WebElement]
-    Version = Tuple[int, int, int, int]
 
-    class CookieProtocol(Protocol):
-        def get_cookies(self):
-            ...
+class CookieProtocol(Protocol):
+    def get_cookies(self):
+        ...
 
-        def add_cookie(self, cookie):
-            ...
+    def add_cookie(self, cookie):
+        ...
 
-        def delete_all_cookies(self):
-            ...
+    def delete_all_cookies(self):
+        ...
 
-    class ScriptExecutionProtocol(Protocol):
-        def execute_script(self, script: str, *args):
-            ...
 
-    class ElementFinderProtocol(Protocol):
-        def find_element_by_tag_name(self, tag_name: str) -> WebElement:
-            ...
+class ScriptExecutionProtocol(Protocol):
+    def execute_script(self, script: str, *args):
+        ...
 
-    class CapabilitiesProtocol(Protocol):
-        @property
-        def capabilities(self) -> Dict[str, Any]:
-            ...
 
-    class WebDriverProtocol(
-        CookieProtocol,
-        CapabilitiesProtocol,
-        ElementFinderProtocol,
-        ScriptExecutionProtocol,
-        Protocol,
-    ):
-        """Minimal typing protocol used by
-        :py:class:`~manen.navigator.NavigatorMixin`.
-        """
+class ElementFinderProtocol(Protocol):
+    def find_element_by_tag_name(self, tag_name: str) -> WebElement:
+        ...
 
-    class InstalledVersionInfo(TypedDict):
-        channel: str
-        os: str
-        release_date: date
-        version: Version
+
+class CapabilitiesProtocol(Protocol):
+    @property
+    def capabilities(self) -> Dict[str, Any]:
+        ...
+
+
+class WebDriverProtocol(
+    CookieProtocol,
+    CapabilitiesProtocol,
+    ElementFinderProtocol,
+    ScriptExecutionProtocol,
+    Protocol,
+):
+    """Minimal typing protocol used by
+    :py:class:`~manen.navigator.NavigatorMixin`.
+    """
+
+
+class InstalledVersionInfo(TypedDict):
+    channel: str
+    os: str
+    release_date: date
+    version: Version

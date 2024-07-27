78314fd39a1d8d756bf827d8f38999b6481d0a16
kodaho <23268819+kodaho@users.noreply.github.com>
2024-07-27 22:09:28 +0200

    Upgrade version to 0.3.0

--- manen/__init__.py
+++ manen/__init__.py
@@ -18,4 +18,4 @@ use the module :py:mod:`~manen.finder` without :py:mod:`~manen.browser` or
 :py:mod:`~manen.page_object_model` without :py:mod:`~manen.browser`.
 """
 
-__version__ = "0.2.1.dev0"
+__version__ = "0.3.0.dev0"

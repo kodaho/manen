c4afc477ada15734e72ba8ff018bddfaa5b79541
kodaho <23268819+kodaho@users.noreply.github.com>
2024-07-28 12:22:42 +0200

    docs: rewrite main module docstring

--- manen/__init__.py
+++ manen/__init__.py
@@ -1,16 +1,16 @@
 """
-:py:mod:`manen` is a module to ease the interaction with Selenium. Its features
-are splitted into four (almost) independent modules:
+:py:mod:`manen` is a module to aiming to enhance developer experience when working on automation
+and scraping project. Its features are splitted into (almost) independent modules:
 
 - :py:mod:`~manen.finder`: define a function :py:func:`~manen.finder.find` in order to
   avoid calling the methods ``find_element_by_*`` or ``find_elements_by_*`` of a Selenium
   :py:class:`~selenium.webdriver.remote.webelement.WebElement`
 - :py:mod:`~manen.browser`: a wrapper around
-  :py:class:`~selenium.webdriver.remote.webdriver.WebDriver` to more easily create
-  instance of web drivers. Only compatible with Chrome and Brave (for now).
+  :py:class:`~selenium.webdriver.remote.webdriver.WebDriver` to create more easily
+  web driver instances. Only compatible with Chrome (for now).
 - :py:mod:`~manen.page_object_model`: an implementation of the design pattern
-  `Page Object Models <https://www.selenium.dev/documentation/en/guidelines_and_recommendations/page_object_models/>`_
-  in order to easily interact with the DOM of a webpage.
+  `Page Object Models <https://www.selenium.dev/documentation/en/guidelines_and_recommendations/page_object_models/>`_,
+  allowing to easily interact with the HTML DOM through classes with attributes and annotations.
 
 Each module is built to be quite independent from each other. For example, you can
 use the module :py:mod:`~manen.finder` without :py:mod:`~manen.browser` or

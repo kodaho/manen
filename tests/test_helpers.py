7409c017b9c704a6342f0756b256d987593c59f5
kodaho <23268819+kodaho@users.noreply.github.com>
2024-09-29 00:13:42 +0200

    fix: remove deprecated tests (#55)

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,43 +1,6 @@
 import pytest
 
-from manen.helpers import extract_integer, version
-
-
-@pytest.mark.parametrize(
-    "expected_str,expected_int",
-    [
-        ("1", 1),
-        ("0", 0),
-        ("1234", 1234),
-        ("01234", 1234),
-    ],
-)
-def test_extract_simple_integer(expected_str, expected_int):
-    assert extract_integer(expected_str) == expected_int
-
-
-@pytest.mark.parametrize(
-    "expected_str,expected_int",
-    [
-        ("1 thing", 1),
-        ("0 thing", 0),
-        ("10 things and more", 10),
-        ("10 things and more...?!#", 10),
-        ("There are 1234new things", 1234),
-    ],
-)
-def test_extract_integer_inside_text(expected_str, expected_int):
-    assert extract_integer(expected_str) == expected_int
-
-
-@pytest.mark.parametrize("float_str,value", [("10,123.23", 10123), ("10.1", 10)])
-def test_extract_integer_from_float(float_str, value):
-    assert extract_integer(float_str) == value
-
-
-@pytest.mark.parametrize("ambiguous_str,value", [("1 thing and 2 others", 1)])
-def test_extract_integer_with_ambiguous_case(ambiguous_str, value):
-    assert extract_integer(ambiguous_str) == value
+from manen.helpers import version
 
 
 @pytest.mark.parametrize(
